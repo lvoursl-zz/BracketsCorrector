043676692012ae0db598b490482c8f58a94c42a4
Yury Makarov <lvoursl@gmail.com>
2015-03-27 21:54:37 +0600

    update main algroithm (rewrite with optimization), UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+bool isBracketInArray(QVector<QString> brackets) {
+    for (int i = 0; i < brackets.length(); i++) {
+        if ((brackets[i] == "(") || (brackets[i] == ")")) return true;
+    }
+
+    return false;
+}
+
+int findLastLeftBracket(QVector<QString> brackets) {
+    int index = -1;
+    for (int i = 0; i < brackets.length(); i++) {
+        if (brackets[i] == "(") index = i;
+    }
+
+    return index;
+}
+
+int findFirstRightBracket(QVector<QString> brackets) {
+    for (int i = 0; i < brackets.length(); i++) {
+        if (brackets[i] == ")") return i;
+    }
+
+    return -1;
+}
+
+
 void MainWindow::on_testButton_clicked()
 {
     if (ui->statementText->toPlainText().isEmpty() != true) {
@@ -32,33 +59,64 @@ void MainWindow::on_testButton_clicked()
             }
         }
 
-        qDebug() << brackets.length() << endl;
+        qDebug() << brackets.length() << endl << isBracketInArray(brackets);
         //qDebug() << statementText.mid(0, 1) << statementText.mid(2, 0);
 
         int randomNum;
         QString randomColor;
 
-        for (int i = 0; i < brackets.length() / 2; i++) {
+        for (int i = 0; i < brackets.length() - 1; i++) {
             randomNum = rand() % 999999 + 111111;
-            randomColor = "#" + QString::number(randomNum);                        
-            brackets[i] = "<font color=\"" + randomColor + "\">" + brackets[i] + "</font>";
-            brackets[brackets.length() - 1 - i] = "<font color=\"" + randomColor + "\">"
-                                                    + brackets[brackets.length() - 1 - i]
-                                                    + "</font>";
+            randomColor = "#" + QString::number(randomNum);
 
-            //ui->statementLabel->setText( brackets[i]);
-            //qDebug() << brackets[i] << brackets[brackets.length() - 1 - i] << endl;
+            if ((brackets[i] == "(") && (brackets[i + 1] == ")")) {
+                 brackets[i] = "<font color=\"" + randomColor + "\"FONT SIZE = 5>" + brackets[i] + "</font>";
+                 brackets[i + 1] = "<font color=\"" + randomColor + "\"FONT SIZE = 5>" + brackets[i + 1] + "</font>";
+            }
+        }
+
+        int lastLeftBracketIndex = -1;
+        int firstRightBrackeIndex = -1;
+
+        int A, B, C;
+        A = 5.6;
+        B = 7.3;
+        C = 0;
+
+        bool G = (A && B) || (B && C);
+        qDebug() << "G" << G;
+
+        while (isBracketInArray(brackets)) {
+            lastLeftBracketIndex = findLastLeftBracket(brackets);
+            firstRightBrackeIndex = findFirstRightBracket(brackets);
+
+            if ((lastLeftBracketIndex != -1) && (firstRightBrackeIndex != -1)) {
+                randomNum = rand() % 999999 + 111111;
+                randomColor = "#" + QString::number(randomNum);
+
+                brackets[lastLeftBracketIndex] = "<font color=\"" + randomColor + "\"FONT SIZE = 5>"
+                                                 + brackets[lastLeftBracketIndex] + "</font>";
+                brackets[firstRightBrackeIndex] = "<font color=\"" + randomColor + "\"FONT SIZE = 5>"
+                                                 + brackets[firstRightBrackeIndex] + "</font>";
+            } else {
+                break;
+            }
+
         }
 
         for (int i = 0; i < brackets.length(); ++i) {
             qDebug() << brackets[i] << endl;
         }
 
+        int length = 0;
         for (int i = 0; i < statementText.length(); i++) {
             if (((statementText.at(i) == '(') || (statementText.at(i) == ')')) && (!brackets.isEmpty())) {
+                length = brackets.first().length();
+
                 statementText = statementText.mid(0, i) + brackets.takeFirst()
                                 + statementText.mid(i + 1, statementText.length());
-                i += 29;
+
+                if (length != 1) i += 35;
                 qDebug() << statementText << endl;
             }
         }
@@ -66,7 +124,7 @@ void MainWindow::on_testButton_clicked()
         ui->statementLabel->setText(statementText);
 
         if (left > right) {
-            ui->resultLabel->setText("<font color=\"#0000ff\">left more than right for " + QString::number(left - right));
+            ui->resultLabel->setText("<font color=\"#0000ff\" FONT SIZE = 5>left more than right for " + QString::number(left - right));
         } else if (right > left) {
             ui->resultLabel->setText("right more than left for " + QString::number(right - left));
         } else if ((right == left) && (left != 0) && (right != 0)) {
@@ -78,3 +136,5 @@ void MainWindow::on_testButton_clicked()
 
     }
 }
+
+
